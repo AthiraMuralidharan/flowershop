4eed504426fc49e48fefc785c607261b03a37903
AthiraMuralidharan <athiramuralidharan99@gmail.com>
2022-04-20 12:12:26 +0530

    first commit

---
+++ credential/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+# app_name='credential'
+urlpatterns=[
+    path('registerview/',views.RegistrationView.as_view(),name='RegistrationView'),
+    path('loginview/',views.LoginView.as_view(),name='LoginView'),
+    path('logoutview/',views.LogoutView.as_view(),name='LogoutView'),
+]
